a4573ea36cf8865735226c6e050d17bc6d8e98fe
Harshad Kapei <harshad.kapei@wohlig.com>
2020-08-08 17:42:13 +0530

    Navbar is created with links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
 
+// files imports
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>MarioPlan</h1>
+        <Navbar />
       </div>
     </BrowserRouter>
   );
