a618390d05898064041f6b663f2c8483ad3b1f63
Harshad Kapei <harshad.kapei@wohlig.com>
2020-08-08 18:33:58 +0530

    instead of repeated list single summary added

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,22 +1,12 @@
 import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <span className="card-tiitle">Project Tittle</span>
-        <p className="grey-text">3rd September , 2am</p>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <span className="card-tiitle">Project Tittle</span>
-        <p className="grey-text">3rd September , 2am</p>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <span className="card-tiitle">Project Tittle</span>
-        <p className="grey-text">3rd September , 2am</p>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
