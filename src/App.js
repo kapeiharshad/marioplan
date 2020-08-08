dda1743accabb6fdbcffb1071115ce2f3dbfcbdb
Harshad Kapei <harshad.kapei@wohlig.com>
2020-08-08 18:02:42 +0530

    dashboard created with projectlist and notification

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 // files imports
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path="/" component={Dashboard}></Route>
+        </Switch>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <span className="card-tiitle">Project Tittle</span>
+        <p className="grey-text">3rd September , 2am</p>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <span className="card-tiitle">Project Tittle</span>
+        <p className="grey-text">3rd September , 2am</p>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <span className="card-tiitle">Project Tittle</span>
+        <p className="grey-text">3rd September , 2am</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
